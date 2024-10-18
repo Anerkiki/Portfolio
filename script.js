479288e3aa60838496cb52c375c0ca8b2b5e6212
Anna Saunders <annacfsaunders@hotmail.com>
2024-10-18 15:10:03 +0100

    little updates

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 fetch('data.json')
     .then(res => res.json())
     .then(data => {
-        const productsBox = document.querySelector('#products')
+        const constName = document.querySelector('#tag')
 
         data.items.forEach(product => {
-            productsBox.innerHTML += `
+            constName.innerHTML += `
                 <div class="product">
                     <h3>${product.name}</h3>
                     <p class="price">£${product.price}</p>
@@ -13,3 +13,18 @@ fetch('data.json')
             `
         })
     })
+
+    const menuButton = document.querySelector('.menu-button')
+    const menuLinks = document.querySelector('nav .links')
+    
+    // menuButton.addEventListener('click', function (){
+        // console.log('clicked on button')
+    // }
+    // )
+    
+    menuButton.addEventListener('click', toggleMobileMenu)
+    
+    function toggleMobileMenu() {
+        console.log('clicked on the button')
+        menuLinks.classList.toggle('open')
+    }
